538ec97b0f99f5aeaeda3486f0909ba5c2387561
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-10 14:03:44 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -3,15 +3,17 @@ let count = 0;
 
 function init() {
   // Write your JavaScript code inside the init() function
-  const key = parseInt(e.detail || e.which);
-  if (code[count] === key){
-    ++count;
-    if (count === code.length){
-      alert("Hurray!");
+  function onKeyDownHandler(e) {
+    const key = parseInt(e.detail || e.which);
+    if (code[count] === key){
+      ++count;
+      if (count === code.length){
+        alert("Hurray!");
+        count = 0;
+      }
+    } else {
       count = 0;
     }
-  } else {
-    count = 0;
   }
 
 }
