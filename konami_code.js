eae9cfa7c05262fe7d7270f28cb7bb0297e30f24
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-10 13:35:52 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,12 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+let count = 0;
 
 function init() {
   // Write your JavaScript code inside the init() function
+  if (code[count] === e.which){
+    ++count;
+  } else {
+    count = 0;
+  }
 
 }
