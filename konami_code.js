05ff28fbc266cbb68cabb606490380fe3bb6277a
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-10 14:14:51 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,5 +2,22 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
+  let count = 0;
+
+  function onKeyDownHandler(e) {
+    e.stopPropagation();
+    const key = parseInt(e.detail || e.which);
+    if (code[count] === key){
+      ++count;
+      if (count === code.length){
+        alert("Hurray!");
+        count = 0;
+      }
+    } else {
+      count = 0;
+    }
+  }
+
+  document.body.addEventListener('keydown', onKeyDownHandler);
 
 }
